c96e78609d280c35889601bfed4c6e4d23bca6ce
arosilier <50966934+arosilier@users.noreply.github.com>
2019-08-29 11:10:57 -0500

    Update files-openclose.cpp

--- files-openclose.cpp
+++ files-openclose.cpp
@@ -1,5 +1,13 @@
+// ENGR 23004 - Class Example 
+// Week 5 - Streams & File I/O
+//
+// files-openclose.c
+// This program will attempt to open both an input and ooutput file and will
+// print an error message if it is unsuccessful
+
 #include <iostream> // required to read/write standard input and output
 #include <fstream> // required to read/write data files
+
 using namespace std;
 
 int main() {
