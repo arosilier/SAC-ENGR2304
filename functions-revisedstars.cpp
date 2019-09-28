9c2b26ba4adb1c3d4aafdf60fe1245aa40febb05
arosilier <50966934+arosilier@users.noreply.github.com>
2019-09-27 23:27:44 -0500

    Add files via upload

---
+++ functions-revisedstars.cpp
@@ -0,0 +1,42 @@
+// ENGR 23004 - Class Example 
+// Week 6 - Functions
+//
+// functions-revisedstars.cpp
+// This program will ask the user to enter five numbers and then
+// print five rows of stars corresponding to the numbers entered.
+// This is the resulting program after "functionalizing" the original 
+// version which is in functions-originalstars.cpp
+
+#include <iostream>
+
+using namespace std;
+
+/* Function Prototypes --------------------------------- */
+void printStars(int number);
+
+/* Main function  --------------------------------- */
+int main() {
+
+    int number1, number2, number3, number4, number5;
+    int counter;
+    
+    cout << "Please enter 5 integers: ";
+    cin >> number1 >> number2 >> number3 >> number4 >> number5;
+    
+    printStars(number1);
+    printStars(number2);
+    printStars(number3);
+    printStars(number4);
+    printStars(number5);
+    
+    cout << endl;
+    
+    return 0;
+}
+
+/* Programmer defined functions  --------------------------------- */
+void printStars(int number) {
+
+	// Fill in this code block with the function definition
+
+}
