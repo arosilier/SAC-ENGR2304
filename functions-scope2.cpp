f12e6f02a1ebaededff279a86a847339309377d7
arosilier <50966934+arosilier@users.noreply.github.com>
2019-09-28 12:27:32 -0500

    Add files via upload

---
+++ functions-scope2.cpp
@@ -0,0 +1,44 @@
+// ENGR 2304 - Class Example 
+// Week 6 - Functions
+//
+// functions-scope2.cpp
+// This program illustrates how the same variable name can 
+// be used in different scopes
+
+#include <iostream>
+
+using namespace std;
+
+// global variables
+int a = 1;
+
+// function prototypes
+void someFunction(int a);
+
+int main()
+{
+    cout << "1: the value of a is "<< a << endl;
+    
+    int a = 2;
+ 
+    cout << "2: the value of a is "<< a << endl;
+   
+    for (int a=0; a<5; a++) {  
+        cout << "3: the value of a is "<< a << endl;
+    }
+
+    cout << "4: the value of a is "<< a << endl;
+    
+    someFunction(a);
+
+    cout << "6: the value of a is "<< a << endl;
+
+    return 0;
+}
+
+void someFunction(int a) {
+    a = 10;
+    cout << "5: the value of a is "<< a << endl;
+    
+    return;
+}
