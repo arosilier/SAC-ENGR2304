94f15f8c300dab6c1fa66d685f0c1096ae3ba54b
arosilier <50966934+arosilier@users.noreply.github.com>
2019-09-29 14:48:54 -0500

    Add files via upload

--- functions-scope2.cpp
+++ functions-scope2.cpp
@@ -14,31 +14,42 @@ int a = 1;
 
 // function prototypes
 void someFunction(int a);
+void anotherFunction();
 
 int main()
 {
-    cout << "1: the value of a is "<< a << endl;
-    
-    int a = 2;
- 
-    cout << "2: the value of a is "<< a << endl;
-   
-    for (int a=0; a<5; a++) {  
-        cout << "3: the value of a is "<< a << endl;
-    }
+  cout << "1: the value of a is "<< a << endl;
 
-    cout << "4: the value of a is "<< a << endl;
-    
-    someFunction(a);
+  int a = 2;
 
-    cout << "6: the value of a is "<< a << endl;
+  cout << "2: the value of a is "<< a << endl;
 
-    return 0;
+  for (int a=0; a<5; a++) { 
+    cout << "3: the value of a is "<< a << endl;
+  }
+
+  cout << "4: the value of a is "<< a << endl;
+
+  someFunction(a);
+
+  cout << "6: the value of a is "<< a << endl;
+
+  anotherFunction();
+
+  cout << "8: the value of a is "<< a << endl;
+  
+  return 0;
 }
 
 void someFunction(int a) {
-    a = 10;
-    cout << "5: the value of a is "<< a << endl;
-    
-    return;
+  a = 10;
+  cout << "5: the value of a is "<< a << endl;
+
+  return;
+}
+
+void anotherFunction() {
+  cout << "7: the value of a is "<< a << endl;
+
+  return;
 }
