16f312c02ed8bf8199d87806faaeef98f723e306
arosilier <50966934+arosilier@users.noreply.github.com>
2019-09-29 20:07:06 -0500

    Add files via upload

--- functions-scopePractice.cpp
+++ functions-scopePractice.cpp
@@ -19,29 +19,29 @@ int yetAnotherFunction(int b);
 // main function
 int main()
 {
-  cout << "1: the value of b is "<< b << endl;
+  cout << "the value of b is "<< b << endl;
 
   int b = 10;
 
-  cout << "2: the value of b is "<< b << endl;
+  cout << "the value of b is "<< b << endl;
 
   for (int b=5; b<10; b++) { 
-    cout << "3: the value of b is "<< b << endl;
+    cout << "the value of b is "<< b << endl;
   }
 
-  cout << "4: the value of b is "<< b << endl;
+  cout << "the value of b is "<< b << endl;
 
   someFunction(b);
 
-  cout << "7: the value of b is "<< b << endl;
+  cout << "the value of b is "<< b << endl;
 
   anotherFunction();
 
-  cout << "9: the value of b is "<< b << endl;
+  cout << "the value of b is "<< b << endl;
   
   b = yetAnotherFunction(b);
   
-  cout << "12: the value of b is "<< b << endl;
+  cout << "the value of b is "<< b << endl;
   
   return 0;
 }
@@ -50,28 +50,28 @@ int main()
 
 void someFunction(int b) {
 
-  cout << "5: the value of b is "<< b << endl;
+  cout << "the value of b is "<< b << endl;
 
   b = 20;
   
-  cout << "6: the value of b is "<< b << endl;
+  cout << "the value of b is "<< b << endl;
 
   return;
 }
 
 void anotherFunction() {
-  cout << "8: the value of b is "<< b << endl;
+  cout << "the value of b is "<< b << endl;
 
   return;
 }
 
 int yetAnotherFunction(int b) {
   
-  cout << "10: the value of b is "<< b << endl;
+  cout << "the value of b is "<< b << endl;
 
-  b = 20;
+  b = 30;
   
-  cout << "11: the value of b is "<< b << endl;
+  cout << "the value of b is "<< b << endl;
 
   return b;
 }
